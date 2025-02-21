8bd0fa2a5d3f26cbaefb5ec11e3e0e73f6a0109c
alcuin88 <alcuin88@gmail.com>
2025-02-21 17:16:28 +0800

    Trying loading screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Suspense } from "react";
-import Loading from "./loading";
 
 export const metadata: Metadata = {
   title: "Money Lending App",
@@ -17,9 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-[bg-[#FAFAFA] text-[#212121]">
         <div className="w-11/12 max-w-7xl my-8 mx-auto">
-          <Suspense fallback={<Loading />}>
-            <main className="font-sans font-normal">{children}</main>
-          </Suspense>
+          <main className="font-sans font-normal">{children}</main>
         </div>
       </body>
     </html>
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <p>Loading</p>;
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
+    </div>
+  );
 }
